0fa54312273c254bf48f86273f5f79da7f1ddc1b
Markus Klund <markukl@stud.ntnu.no>
2024-06-15 17:39:22 +0200

    Remove: removed unneccessary lines in navbar component #1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,6 @@ export default function Navbar() {
         <a href="#about">About Me</a>
         <a href="#about">Log In</a>
         <a href="#home">Home</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"></i>
-        </a>
     </div>
     );
 }
