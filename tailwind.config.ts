4d0090ce0b7b76da32be254b6bb89336f3ebc7c4
Markus Klund <markus.klund@hotmail.com>
2024-12-19 13:23:03 +0100

    feat: setup project

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: '#000',
+          100: '#000319'
+        }
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
