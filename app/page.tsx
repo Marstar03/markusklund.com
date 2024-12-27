50359484609cb3c58cc697fb1caaa68273e78b07
Markus Klund <markus.klund@hotmail.com>
2024-12-27 13:10:34 +0100

    feat(ui): add experience section (#10)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -15,6 +16,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Experience />
       </div>
     </main>
   );
